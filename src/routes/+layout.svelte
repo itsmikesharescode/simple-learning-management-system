4db9211a6a18ba6d1bd9cf77dedf57422b984008
itsmikesharescode <mikejohneviota@gmail.com>
2024-01-10 18:31:24 +0800

    fixing animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,15 +10,18 @@
 	import Loader from '$lib/Components/Loader.svelte';
 
 	initializeStores();
-			
+	
+	import loading from "$lib/Components/Images/loading.svg";
 
 </script>
 
 <Toast position="tr" rounded="rounded-lg" spacing="space-x-2" padding="py-3 px-5" max={5} transitions={true} buttonDismiss="rounded-full text-red-500"/>
 
+<div class="fixed bottom-0 top-0 left-0 right-0 -z-10">
+	<img src={loading} alt="" class="fixed h-full" />
+</div>
 
-
-<div class="bg-[url('./lib/Components/Images/loading.svg')] ">
+<div class="">
 	{#if $navigating}
 		<Loader />
 	{/if}
